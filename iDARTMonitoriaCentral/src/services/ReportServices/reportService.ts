28e66391e9308e55f22ad42b33b7e1eb5bee1981
Colaco Nhongo <colaco.nhango@fgh.org.mz>
2022-06-07 18:37:31 +0200

    Add jsPDF for Report
    Add Lista de Pacientes Referidos Report

--- iDARTMonitoriaCentral/src/services/ReportServices/reportService.ts
+++ iDARTMonitoriaCentral/src/services/ReportServices/reportService.ts
@@ -1,17 +1,17 @@
-import { useRepo } from 'pinia-orm';
+import moment from 'moment';
 import api from 'src/services/apiService/apiService';
-import { alert } from '../../components/Shared/Directives/Plugins/Dialog/dialog';
-
 
 export default {
   // Axios API call
   getReferedPatientsReport(offset: number) {
-      return api()
-        .get('refered_patients_vw?offset=' + offset + '&limit=100')
-        .then((resp) => {
-          console.log(resp.data)
-          return resp.data
-        });
+    return api()
+      .get('refered_patients_vw?offset=' + offset + '&limit=100')
+      .then((resp) => {
+        return resp.data;
+      });
   },
 
+  getFormatDDMMYYYY(date) {
+    return moment(date).format('DD-MM-YYYY');
+  },
 };
