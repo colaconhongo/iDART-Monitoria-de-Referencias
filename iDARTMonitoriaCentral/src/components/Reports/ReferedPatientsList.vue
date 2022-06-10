af1dbce238fcd36872e58f84390cf401f28109e1
Colaco Nhongo <colaco.nhango@fgh.org.mz>
2022-06-10 13:11:14 +0200

    Update Excel Report
    - Add Byte Image
    - Add DownloadEXCEL

--- iDARTMonitoriaCentral/src/components/Reports/ReferedPatientsList.vue
+++ iDARTMonitoriaCentral/src/components/Reports/ReferedPatientsList.vue
@@ -28,16 +28,15 @@ import { ref } from 'vue';
 import FiltersInput from 'src/components/Reports/Shared/FiltersInput.vue';
 import Bar from 'src/components/Shared/Bar.vue';
 import reportReferidos from 'src/services/ReportServices/referidos/reportReferidos';
-import moment from 'moment'
+import moment from 'moment';
 import reportExcel from 'src/services/ReportServices/faltosos/reportExcelJs';
-
 
 const title = ref('Lista de Pacientes Referidos para Outras Farmácias');
 
 const generateReport = (params) => {
   console.log(params.value.fileType);
-    if (params.value.fileType === 'PDF') {
-      reportReferidos.downloadPDF(
+  if (params.value.fileType === 'PDF') {
+    reportReferidos.downloadPDF(
       params.value.clinic.clinicname,
       params.value.province.name,
       moment(params.value.startDate).format('DD/MM/YYYY'),
@@ -45,11 +44,12 @@ const generateReport = (params) => {
       params
     );
   } else {
-    reportFaltososARV.downloadExcel(
-      'CS Nicoadala',
-      'Zambézia',
-      '01-01-2020',
-      '02-01-2020'
+    reportReferidos.downloadExcel(
+      params.value.clinic.clinicname,
+      params.value.province.name,
+      moment(params.value.startDate).format('DD/MM/YYYY'),
+      moment(params.value.endDate).format('DD/MM/YYYY'),
+      params
     );
   }
 };
